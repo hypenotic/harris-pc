b5a4902bdc79f528dd6a742892eb78dc327ab95b
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-02-28 16:55:57 -0500

    Updates navigation

--- custom/themes/pulsecheck/header.php
+++ custom/themes/pulsecheck/header.php
@@ -5,8 +5,20 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 <link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/dist/images/favicon.ico" type="image/x-icon">
 <title><?php bloginfo( 'name' ); ?></title>
-    
 <?php wp_head(); ?>
-
 </head>
 <body>
+  <nav class="blue-bg">
+    <div class="nav-wrapper blue-bg">
+      <a href="#" class="brand-logo">PulseCheck</a>
+      <menu>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="">Item 1</a></li>
+          <li><a href="">Item 2</a></li>
+          <li><a href="">Item 3</a></li>
+          <li><a href="">Item 4</a></li>
+          <li><a href="">Item 5</a></li>
+        </ul>
+      </menu>
+    </div>
+  </nav>
