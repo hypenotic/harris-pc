1d3619f7242aec0bbb3144e76a7f98e4bb1f063d
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-09 16:59:22 -0500

    Fixes mobile menu trigger

--- custom/themes/pulsecheck/src/js/init.js
+++ custom/themes/pulsecheck/src/js/init.js
@@ -1,12 +1,11 @@
 $ = jQuery;
+// Activate Mobile Nav
+function openMobileNav() {
+	$(".navbar-fixed").toggleClass( "nav-menu-open" );
+	$("body").toggleClass( "no-scroll" );
+	$("#nav-icon").toggleClass('open');
+}
 
 $(function() {
-    // Activate Mobile Nav
-    function openMobileNav() {
-    	$(".navbar-fixed").toggleClass( "nav-menu-open" );
-    	$("body").toggleClass( "no-scroll" );
-    	$("#nav-icon").toggleClass('open');
-    }
-
     $('.carousel.carousel-slider').carousel({fullWidth: true,indicators: true});
 });
