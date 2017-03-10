dc83dbb8baf4a41cc6d269692b030c1a62e08ecd
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-10 13:53:39 -0500

    Adds Clients & Partners metaboxes. Updates blog templates and styles.

--- custom/themes/pulsecheck/single.php
+++ custom/themes/pulsecheck/single.php
@@ -3,6 +3,11 @@
 <section>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
+    <div class="container">
+    	<h1><?php the_title();?></h1>
+    	<div><?php the_content();?></div>
+    </div>
+
     <?php endwhile; else : ?>
         <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
     <?php endif; ?>
--- custom/themes/pulsecheck/template-blog.php
+++ custom/themes/pulsecheck/template-blog.php
@@ -3,22 +3,55 @@
 * Template Name: Blog
 */
 get_header(); ?>
-<?php // Loop starts
-if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+<?php
 	$heading         = rwmb_meta( 'rw_banner_heading' );
 	$subhead         = rwmb_meta( 'rw_banner_subheading' );
 ?>
 <section class="blog-header" id="skip-to-content">
-	<h1>The Pulse</h1>
+	<div class="container">
+		<h1>The Pulse</h1>
+	</div>
 </section>
 
-<section >
-    
+<section class="blog-posts">
+    <div class="container">
+    	<?php 
+    	// the query
+    	$args = array(
+    	    'post_type' => array( 'post', 'case-study', 'article' ),
+    	    'posts_per_page'=> '10',
+    	    'order' => 'DESC'
+    	);
+    	$the_query = new WP_Query( $args ); ?>
+
+    	<?php if ( $the_query->have_posts() ) : ?>
+
+    		<!-- pagination here -->
+
+    		<!-- the loop -->
+    		<?php while ( $the_query->have_posts() ) : $the_query->the_post(); 
+    			// You might have to structure Case Studies a bit differently, and pull the $heading custom field instead of the_title...
+    			$the_heading         = rwmb_meta( 'rw_cs_banner_heading' );
+    			if ($the_heading) {
+    				$the_title = $the_heading;
+    			} else {
+    				$the_title = get_the_title();
+    			}
+    		?>
+    			<a href="<?php the_permalink();?>"><h2><?php echo $the_title; ?></h2></a>
+    			<div><?php the_excerpt();?></div>
+    		<?php endwhile; ?>
+    		<!-- end of the loop -->
+
+    		<!-- pagination here -->
+
+    		<?php wp_reset_postdata(); ?>
+
+    	<?php else : ?>
+    		<p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+    	<?php endif; ?>
+
+    </div>
 </section>
-
-
-<?php endwhile; else : ?>
-    <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
-<?php endif; ?>
 <?php get_footer(); ?>
 
