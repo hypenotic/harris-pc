69fdefb771faa25b3f6b3a90949bf7024af3a328
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-28 10:54:09 -0400

    Removes spaces from style and script enqueue files

--- custom/themes/pulsecheck/includes/functions/enqueue-script.php
+++ custom/themes/pulsecheck/includes/functions/enqueue-script.php
@@ -1,16 +1,12 @@
 <?php
-
 /**
 * Enqueue scripts
 */
-
 function my_scripts() {
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('swiper-jq', get_template_directory_uri() . '/dist/js/swiper.jquery.min.js', array('jquery'), '', true);
 	wp_enqueue_script('swiper', get_template_directory_uri() . '/dist/js/swiper.min.js', array('jquery'), '', true);
   	wp_enqueue_script('app', get_template_directory_uri() . '/dist/js/app.min.js', array('jquery'), '', true);
 }
-
 add_action( 'wp_enqueue_scripts', 'my_scripts' );
-
 ?>
--- custom/themes/pulsecheck/includes/functions/enqueue-style.php
+++ custom/themes/pulsecheck/includes/functions/enqueue-style.php
@@ -1,9 +1,7 @@
  <?php
-
 /**
 * Enqueue styles
 */
-
 function my_styles() {
 	wp_enqueue_style( 'material', 'https://fonts.googleapis.com/icon?family=Material+Icons', false);
 	wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,700,700i', false );
@@ -16,7 +14,5 @@ function my_styles() {
 	wp_register_style('style', get_template_directory_uri() . '/dist/css/style.css');
  	wp_enqueue_style( 'style' );
 }
-
 add_action('wp_enqueue_scripts', 'my_styles');
-
 ?>
