100604d31446a55ebc5370f80428266779edc877
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-27 20:55:02 -0400

    Adds Coming Soon template

--- custom/themes/pulsecheck/footer.php
+++ custom/themes/pulsecheck/footer.php
@@ -1,32 +1,34 @@
-<footer class="top-footer base-padding no-lr-padding no-tb-padding">
-	<div class="row">
-		<div class="col s12 m6">
-			<div id="contact-map">
-			    
-			</div>
-		</div>	
-		<div class="col s12 m6">
-			<div class="contact-content">
-				<h3>Get in touch!</h3>
-				<p>PulseCheck<br/>
-				10275 West Higgins Road<br/>
-				Suite 250<br/>
-				Rosemont, IL<br/>
-				60018</p>
-				<p>844-240-9554<br/>
-				press 1 for support, 3 for sales</p>
-				<p><a href="#" class="underline">info@picis.com</a></p>
-				<p><a href="#" class="underline">Linkedin </a></p>
+<?php if (!is_page_template( 'template-cs.php' )) {?>
+	<footer class="top-footer base-padding no-lr-padding no-tb-padding">
+		<div class="row">
+			<div class="col s12 m6">
+				<div id="contact-map">
+				    
+				</div>
+			</div>	
+			<div class="col s12 m6">
+				<div class="contact-content">
+					<h3>Get in touch!</h3>
+					<p>PulseCheck<br/>
+					10275 West Higgins Road<br/>
+					Suite 250<br/>
+					Rosemont, IL<br/>
+					60018</p>
+					<p>844-240-9554<br/>
+					press 1 for support, 3 for sales</p>
+					<p><a href="#" class="underline">info@picis.com</a></p>
+					<p><a href="#" class="underline">Linkedin </a></p>
+				</div>
 			</div>
 		</div>
-	</div>
-</footer>
+	</footer>
 
-<footer class="bottom-footer">
-	<p>Resources & Support</p>
-	<a class="underline" href="#">Login</a>
-</footer>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC2vLY27PcE_ZpN7YZGzDWX6BSYTRuRHgY"></script>
+	<footer class="bottom-footer">
+		<p>Resources & Support</p>
+		<a class="underline" href="#">Login</a>
+	</footer>
+	<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyC2vLY27PcE_ZpN7YZGzDWX6BSYTRuRHgY"></script>
+<?php } ?>
 <?php wp_footer(); ?>
 </body>
 </html>
