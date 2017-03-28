8ea907341c432d13a9349a8d2a2fa62cf1e861df
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-28 11:13:49 -0400

    Updates styles and script enqueues

--- custom/themes/pulsecheck/functions.php
+++ custom/themes/pulsecheck/functions.php
@@ -1,7 +1,7 @@
 <?php
 //Load custom functions
-require_once('includes/functions/enqueue-style.php');
 require_once('includes/functions/enqueue-script.php');
+require_once('includes/functions/enqueue-style.php');
 require_once('includes/functions/add-menu-support.php');
 require_once('includes/functions/register-menu.php');
 // Load MetaBox.io files
