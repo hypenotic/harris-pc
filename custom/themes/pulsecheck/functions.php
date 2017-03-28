45d8d2776d0216fe2e734b76f9edc435fd3d21cf
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-03-28 11:04:54 -0400

    Updates header

--- custom/themes/pulsecheck/functions.php
+++ custom/themes/pulsecheck/functions.php
@@ -4,7 +4,6 @@ require_once('includes/functions/enqueue-style.php');
 require_once('includes/functions/enqueue-script.php');
 require_once('includes/functions/add-menu-support.php');
 require_once('includes/functions/register-menu.php');
-
 // Load MetaBox.io files
 require_once('includes/metabox/cpt-page.php');
 require_once('includes/metabox/cpt-product.php');
