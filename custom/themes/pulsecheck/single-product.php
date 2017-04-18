4c5afbdabbe4b7f27acb5d2c3df0f91d0b5fb9b1
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-04-18 10:00:54 -0400

    Updates styles

--- custom/themes/pulsecheck/single-product.php
+++ custom/themes/pulsecheck/single-product.php
@@ -279,26 +279,56 @@ if ( ! empty( $recogs ) ) { ?>
 <section class="product-support-resources base-padding">
 	<div class="wide-container">
 		<h3 class="grey-text ta-center">Make the case to your team</h3>
-		<div class="support-resources-container flex-container">
-			<div>
-				<p>Flyer</p>
-				<h4>Perioperative Suite</h4>
-				<div class="resource_separator"></div>
-				<div class="resource_blurb">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, iusto.</p>
-				</div>
-				<a href="#" class="btn no-shadow">View</a>
-			</div>
-			<div>
-				<p>Case Study</p>
-				<h4>Streamlining Processes, Realizing Cost Savings</h4>
-				<div class="resource_separator"></div>
-				<div class="resource_blurb">
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis, iusto.</p>
-				</div>
-				<a href="#" class="btn no-shadow">View</a>
-			</div>
-		</div>
+
+			<?php
+
+			if ($is_child == false) {
+				$the_term = $post_slug;
+			} else {
+				$parent_post = get_post($post->post_parent); 
+				$parent_slug = $parent_post->post_name;
+				$the_term = $parent_slug;
+			}
+			 // Check if there are any resources
+			$args = array(
+			    'post_type' => 'resource',
+			    'tax_query' => array(
+			        array(
+			            'taxonomy' => 'products',
+			            'field'    => 'slug',
+			            'terms'    => $the_term,
+			        ),
+			    ),
+			    'posts_per_page'=> '10',
+			    'orderby' => 'menu_order',
+			    'order' => 'ASC'
+			);
+			
+			$loop = new WP_Query( $args );
+			if ($loop->have_posts()){
+			    // $count = 0;
+
+			?>
+			    <div class="support-resources-container flex-container">
+			    <?php while ( $loop->have_posts() ) : $loop->the_post(); 
+			        $res_title     = rwmb_meta( 'rw_cs_banner_heading' );
+			        $res_file    = rwmb_meta( 'rw_cs_file' );
+			        // $count++;
+			        $res_type =  wp_get_post_terms($post->ID, 'resource-type');
+			        // print_r($res_type[0]->name);
+			    ?>
+			        <div>
+			        	<p><?php echo $res_type[0]->name;?></p>
+			        	<h4><?php echo $res_title;?></h4>
+			        	<div class="resource_separator"></div>
+			        	<div class="resource_blurb">
+			        		<p><?php the_excerpt();?></p>
+			        	</div>
+			        	<a href="<?php echo $res_file;?>" target="_blank" class="btn no-shadow">View</a>
+			        </div>
+			    <?php endwhile; wp_reset_postdata(); ?>
+			    </div>
+			<?php } ?>
 	</div>
 </section>
 
