925c7c2897758f2deb65cdfd8ad56e4bd7a0d98e
Emily Dela Cruz <delacruz.emily@gmail.com>
2017-05-15 11:49:59 -0400

    Updates favicon

--- custom/themes/pulsecheck/header.php
+++ custom/themes/pulsecheck/header.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
 <?php wp_head(); ?>
-<link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/dist/images/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="<?php echo get_template_directory_uri();?>/favicon.ico" type="image/x-icon">
 <title><?php bloginfo( 'name' ); ?></title>
 </head>
 <body>
